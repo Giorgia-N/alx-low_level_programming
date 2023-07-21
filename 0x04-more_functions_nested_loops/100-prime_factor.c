47c3f4253b1968aec5ff0b6280eadbf1c74756eb
Giorgia <nthabigiorgia19@gmail.com>
2023-07-21 16:46:22 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Prints out thr prime factor of the number 612852475143
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	unsigned long int a, b = 612852475143;
+
+	for (a = 3; a < 782849; a = a + 2)
+	{
+		while ((b % a == 0) && (b != a))
+			b = b / a;
+	}
+	printf("%lu\n", b);
+	return (0);
+}
