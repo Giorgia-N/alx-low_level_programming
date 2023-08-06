82862cfb7fcdccdc5d6c90d5d45044caaddef9a2
Giorgia <nthabigiorgia19@gmail.com>
2023-08-06 12:19:35 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - prints the mane of the file
+ * @argv: array
+ * @argc: count
+ * Return: Alwats 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
