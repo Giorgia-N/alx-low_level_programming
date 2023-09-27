4ffeee900941e1ca1834e761981cb8b75ab6747f
Giorgia-N <nthabigiorgia19@gmail.com>
2023-09-27 13:52:27 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * _puts_recursion - A function that prints a string
+ * @s: input
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+The seconde chance on recursion
