33e05ca8c59c79e33b510462232811458b448209
Giorgia-N <nthabigiorgia19@gmail.com>
2023-10-19 15:10:52 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,9 +15,9 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0](nil)\n");
 		else
-			printf("[%u]%s\n", h->len, h->str);
-		h = h->next;
+			printf("[%d] %s\n", h->len, h->str);
 		a++;
+		h = h->next;
 	}
 	return (a);
 }
